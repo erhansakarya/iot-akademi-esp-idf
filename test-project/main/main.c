2230fd430ad4bfd824f2d23a8200bf53b5ac6e78
unknown <erhansakaryaa@gmail.com>
2024-11-24 20:05:17 +0300

    freertos task notifications #0

--- test-project/main/main.c
+++ test-project/main/main.c
@@ -8,9 +8,10 @@
 #include "esp_heap_caps.h"
 #include "esp_random.h"
 
-//#define TAG_1 "LED_TASK_1"
-
 #define BUFFER_SIZE 1024
+
+TaskHandle_t sensor_task_handle;
+TaskHandle_t network_task_handle;
 
 void sensor_task(void *pvParameters)
 {
@@ -19,6 +20,12 @@ void sensor_task(void *pvParameters)
         ESP_LOGI("SENSOR_TASK", "Sensor task executing for pin %d", *(uint8_t *)pvParameters);
         // Log the task core id
         ESP_LOGI("SENSOR_TASK", "Sensor task core id: %d", xPortGetCoreID());
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        
+        // NOTE: Send task notification to network task
+        xTaskNotifyGive(network_task_handle);
+
         vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
 }
@@ -30,7 +37,10 @@ void network_task(void *pvParameters)
         ESP_LOGI("NETWORK_TASK", "Network task executing");
         // Log the task core id
         ESP_LOGI("NETWORK_TASK", "Network task core id: %d", xPortGetCoreID());
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        
+        // NOTE: Wait for task notification from sensor task
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+        ESP_LOGI("NETWORK_TASK", "Received task notification from sensor task");
     }
 }
 
@@ -38,41 +48,13 @@ void app_main(void)
 {   
     esp_log_level_set("*", ESP_LOG_INFO);
 
-    uint8_t pin_number_1 = 12;
     uint8_t pin_number_2 = 13;
 
-    TaskHandle_t sensor_task_1_handle;
-    TaskHandle_t sensor_task_2_handle;
-    TaskHandle_t network_task_handle;
-
-    xTaskCreate(&sensor_task, "SENSOR_TASK_1", 2048, (void *)&pin_number_1, 5, &sensor_task_1_handle);
-    //xTaskCreate(&sensor_task, "SENSOR_TASK_2", 2048, (void *)&pin_number_2, 5, &sensor_task_2_handle);
-    xTaskCreatePinnedToCore(&sensor_task, "SENSOR_TASK_2", 2048, (void *)&pin_number_2, 5, &sensor_task_2_handle, 0);
-    //xTaskCreate(&network_task, "NETWORK_TASK", 2048, NULL, 5, &network_task_handle);
+    xTaskCreatePinnedToCore(&sensor_task, "SENSOR_TASK_2", 2048, (void *)&pin_number_2, 5, &sensor_task_handle, 0);
     xTaskCreatePinnedToCore(&network_task, "NETWORK_TASK", 2048, NULL, 5, &network_task_handle, 1);
-
-    vTaskDelay(5000 / portTICK_PERIOD_MS);
-    vTaskSuspend(sensor_task_1_handle);
-    ESP_LOGI("MAIN", "Sensor task 1 suspended");
-
-    vTaskDelay(5000 / portTICK_PERIOD_MS);
-    vTaskResume(sensor_task_1_handle);
-    ESP_LOGI("MAIN", "Sensor task 1 resumed");
-
-    vTaskDelay(5000 / portTICK_PERIOD_MS);
-    vTaskDelete(sensor_task_1_handle);
-    ESP_LOGI("MAIN", "Sensor task 1 deleted");
-
-    eTaskState state = eTaskGetState(sensor_task_1_handle);
-    ESP_LOGI("MAIN", "Sensor task 1 state: %d", state);
 
     while (1)
     {
-        /*
-        UBaseType_t free_heap_size = uxTaskGetStackHighWaterMark(sensor_task_1_handle);
-        ESP_LOGI("MAIN", "Free heap size: %d for sensor task 1", free_heap_size);
-        */
-
         vTaskDelay(5000 / portTICK_PERIOD_MS);
     }
 }
